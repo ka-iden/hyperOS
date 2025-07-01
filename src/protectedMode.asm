2994969cb62924edffe061d00c3287fb3a3647fa
Kaiden B. <kaidenchrisbird@gmail.com>
2025-07-01 18:46:42 +1000

    Enter protected mode!

---
+++ src/protectedMode.asm
@@ -0,0 +1,126 @@
+; Entering Protected Mode
+; Created: 1/07/2025
+; Last Updated: 1/07/2025
+
+	use16 ; Use 16-bit mode, aka real mode
+	org 0x7c00 ; Set the origin to 0x7c00
+
+	jmp startReal ; jump over labels and instructions for printing
+
+%include "src/print16.asm"
+
+startReal: ; Any code that needs to be run once goes below here and above the loop label.
+
+	mov ah, 0x00 ; Tell BIOS Interrupt to set video mode
+	mov al, 0x03 ; 80x25 @ 16 colour mode
+	int 0x10 ; Call BIOS Interrupt
+
+	mov ah, 0x0b ; Tell BIOS Interrupt to set colour registers
+	mov bh, 0x01 ; Palette colour ID
+	mov bl, 0x03 ; Colour value when BH is 0, Palette value when BH = 1
+	int 0x10 ; Call BIOS Interrupt
+
+	mov ah, 0x09 ; Tell BIOS Interrupt to set text colour
+	mov bh, 0x00 ; Display page
+	mov bl, 0x04 ; Colour of text
+	int 0x10 ; Call BIOS Interrupt
+
+	mov ah, 0x0e ; TTY (TeleTYpewriter) mode
+
+	mov bx, bits16 ; Function to print: 'Currently in 16-bit mode.'
+	call sprintLn16 ; Call the print function
+
+	; Time to enter protected mode.
+	mov [bootDisk], dl
+
+	cli ; Clear interrupts
+	lgdt [GDTDescriptor] ; Move the GDT descriptor into the special gdtr register
+	mov eax, cr0 ; Move control register to eax
+	or eax, 1 ; Flip pmode bit
+	mov cr0, eax ; Move eax back into control register
+	jmp CODE_SEG:startProtected ; Perform far jump (jmp to another segment)
+
+bits16: db 'Currently in 16-bit mode.', 0
+
+GDTStart: ; Must be at the end of real mode code.
+nullDescriptor:
+	dd 0x0, 0x0 ; First two bytes are null
+
+codeDescriptor:
+	dw 0xffff ; Define the first 16 bits of the 20-bit code segment limit
+	db 0x0, 0x0, 0x0 ; Define the 24 bits of the 32-bit base of the code segment
+	
+	; The first bit in this byte describes that there is a code segment present.
+	; The second and third bit describe the privilege as "ring", or 00, which is the highest
+	; privilege.
+	; The fourth bit describes the current segment is the code segment.
+	; The fifth bit describes that the segment contains code, part of the type flags.
+	; The sixth bit describes that the segments are non-conforming, meaning codein these
+	; segments can be executed from lower privilged segments, and since we are at the highest
+	; privilege, we should set this to 0.
+	; The seventh bit describes that constants are readable by our instructions.
+	; The last bit describes that the code here is managed by the CPU, also known as the
+	; Accessed flag.
+	db 0b10011010
+	
+	; The first bit describes that we are able to use the whole 4 gigabytes of memory, also
+	; known as the granularity bit.
+	; The second bit describes that the segment will use 32 bit memory.
+	; Since we are not using 64-bit (yet!) and AVL, the third and fourth bit are set to 0.
+	; I also do not know what AVL does. I can't find it on google either.
+	; The last four bits describe the last 4 bits of the 20-bit limit of the code segment, set
+	; to 0xf to give us the maximum available limit.
+	db 0b11001111
+	
+	db 0x0 ; Define the last 8 bits of the 32-bit base of the code segment
+
+dataDescriptor:
+	dw 0xffff ; Same as above, except we change a few things.
+	db 0x0, 0x0, 0x0
+	; This is different, since we need to change the type flags
+	; The fifth bit is set to 0 to indicate that this is not code, and that it is the data
+	; segment.
+	; Since the fifth bit is now data, the sixth bit is now the direction flag, which means
+	; which way the data expands, and we want it to expand upwards, since we are starting the
+	; segment at 0, we want it to expand upwards, so we set the bit to 0.
+	; Since the fifth bit is now data, the seventh bit is now the writable flag, which allows
+	; data to be edited, and we want our data to be writable, so we set it to 1.
+	db 0b10010010
+	
+	db 0b11001111
+	db 0x0
+
+GDTEnd:
+
+GDTDescriptor:
+	dw GDTEnd - GDTStart - 1 ; Last byte in GDT table
+	dd GDTStart ; Start of GDT table
+
+CODE_SEG equ codeDescriptor - GDTStart
+DATA_SEG equ dataDescriptor - GDTStart
+
+	use32 ; Use 32-bit mode, aka protected mode
+
+startProtected:
+	mov esi, bits32 ; Move the label of our message into esi
+	mov edi, 0xb8000 ; Video memory in 32-bit mode starts at 0xb8000
+	add edi, 160 * 1 ; Skip the first line to preserve our original message
+
+.print_line:
+	lodsb
+	cmp al, 0
+	je .done
+	mov ah, 0x07 ; Set the colour to light grey, to match the 16-bit printing.
+	mov [edi], ax
+	add edi, 2
+	jmp .print_line
+
+.done:
+	jmp $
+	
+bits32 db "Now in 32-bit mode!", 0
+
+bootDisk: db 0
+
+	times 510 - ($-$$) db 0 ; Fills empty space with 0s
+	dw 0xaa55 ; Boot sector sig
